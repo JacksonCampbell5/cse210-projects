ab312aa9995936fb92166f6c5c65e14456d12ed3
JacksonCampbell5 <jcampbell6@byupathway.edu>
2026-01-28 19:28:20 -0700

    no comment

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,4 +1,4 @@
-// Alowed the user to input their own scripture reference and text
+// Added an option for the user to input their own scripture reference and text
 // Only selects to hide words that are not already hidden
 using System;
 
@@ -6,23 +6,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Input book: ");
-        string book = Console.ReadLine();
-        Console.Write("Input chapter: ");
-        string chapter = Console.ReadLine();
-        Console.Write("Input starting verse: ");
-        string startVerse = Console.ReadLine();
-        Console.Write("Input ending verse (leave blank if only one verse): ");
-        string endVerse = Console.ReadLine();
+        string option;
+        string book;
+        string chapter;
+        string startVerse;
+        string endVerse;
+        string text; 
+        do
+        {
+            Console.Write("Would you like to input your own verse? (yes/no): ");
+            option = Console.ReadLine().ToLower();
+        } while (option != "yes" && option != "no");
+
+        if (option == "yes")
+        {
+            Console.Write("Input book: ");
+            book = Console.ReadLine();
+            Console.Write("Input chapter: ");
+            chapter = Console.ReadLine();
+            Console.Write("Input starting verse: ");
+            startVerse = Console.ReadLine();
+            Console.Write("Input ending verse (leave blank if only one verse): ");
+            endVerse = Console.ReadLine();
+            Console.Write("Input scripture text: ");
+            text = Console.ReadLine();
+;
+        }
+        else
+        {
+            book = "Proverbs";
+            chapter = "3";
+            startVerse = "5";
+            endVerse = "6";
+            text = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths.";
+        }
+
         Reference reference = new Reference( book,int.Parse(chapter),int.Parse(startVerse));
         if (endVerse != "")
         {
             reference = new Reference( book,int.Parse(chapter),int.Parse(startVerse),int.Parse(endVerse));
         }
-
-        Console.Write("Input scripture text: ");
-        string text = Console.ReadLine();
         Scripture scripture = new Scripture(reference, text);
+
         Console.Clear();
         Console.WriteLine(scripture.GetDisplayText());
         Console.WriteLine("Press enter to continue or type 'quit' to finish:" );
